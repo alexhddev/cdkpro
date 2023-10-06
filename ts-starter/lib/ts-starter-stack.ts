bba08c5e1d2ae8382dcce4f9012a369a57f599ab
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-10-06 18:33:18 +0300

    Ts intrinsic functions practice

--- ts-starter/lib/ts-starter-stack.ts
+++ ts-starter/lib/ts-starter-stack.ts
@@ -1,12 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { aws_s3 } from 'aws-cdk-lib'
+import { aws_s3, Fn } from 'aws-cdk-lib'
 
 export class TsStarterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const suffix = this.initializeSuffix();
+
     const bucket = new aws_s3.Bucket(this, 'TsBucket', {
+      bucketName: `cool-bucket-${suffix}`,
       lifecycleRules: [
         {
           expiration: cdk.Duration.days(3)
@@ -19,6 +22,12 @@ export class TsStarterStack extends cdk.Stack {
         value: bucket.bucketName
       }
       )
+  }
+
+  private initializeSuffix(){
+    const shortStackId = Fn.select(2, Fn.split('/', this.stackId))
+    const suffix = Fn.select(4, Fn.split('-', shortStackId))
+    return suffix;
   }
 
 }
