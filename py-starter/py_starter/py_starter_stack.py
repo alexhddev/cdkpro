2e2885db1c96a605c368b310e6cfed133acc8f71
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-10-06 18:12:38 +0300

    Python intrinsic functions practice

--- py-starter/py_starter/py_starter_stack.py
+++ py-starter/py_starter/py_starter_stack.py
@@ -2,8 +2,8 @@ from aws_cdk import (
     Stack,
     aws_s3 as s3,
     Duration,
-    CfnOutput
-
+    CfnOutput,
+    Fn
 )
 from constructs import Construct
 
@@ -12,7 +12,11 @@ class PyStarterStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        bucket = s3.Bucket(self, "PyBucket", lifecycle_rules=[
+        suffix = self.__initialize_suffix()
+
+        bucket = s3.Bucket(self, "PyBucket",
+            bucket_name=f"cool-bucket-{suffix}",
+            lifecycle_rules=[
             s3.LifecycleRule(
                 expiration=Duration.days(3)
             )
@@ -22,4 +26,7 @@ class PyStarterStack(Stack):
                   value=bucket.bucket_name
                   )
 
-
+    def __initialize_suffix(self):
+        short_stack_id = Fn.select(2, Fn.split('/', self.stack_id))
+        suffix = Fn.select(4, Fn.split('-', short_stack_id))
+        return suffix
