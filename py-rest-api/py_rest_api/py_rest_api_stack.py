eaf9e88761543b7edd1bec0455357f1e2a186442
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-10-12 13:46:46 +0300

    Py api lambda

--- py-rest-api/py_rest_api/py_rest_api_stack.py
+++ py-rest-api/py_rest_api/py_rest_api_stack.py
@@ -1,7 +1,7 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_apigateway,
+    aws_lambda
 )
 from constructs import Construct
 
@@ -10,10 +10,18 @@ class PyRestApiStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        empl_lambda = aws_lambda.Function(
+            self,
+            "EmplLambda",
+            runtime=aws_lambda.Runtime.PYTHON_3_11,
+            code=aws_lambda.Code.from_asset("services"),
+            handler="index.handler",
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "PyRestApiQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        api = aws_apigateway.RestApi(self, "Py-Empl-Api")
+        empl_resource = api.root.add_resource("empl")
+
+        empl_lambda_integration = aws_apigateway.LambdaIntegration(empl_lambda)
+        empl_resource.add_method("GET", empl_lambda_integration)
+        empl_resource.add_method("POST", empl_lambda_integration)
+
