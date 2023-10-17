8ce5a995fcf1876b6adce15309317049f3b6b861
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-10-17 17:18:57 +0300

    [Ts] CDK metric alarms

--- ts-cw-metrics/lib/ts-cw-metrics-stack.ts
+++ ts-cw-metrics/lib/ts-cw-metrics-stack.ts
@@ -1,16 +1,44 @@
 import * as cdk from 'aws-cdk-lib';
+import { Alarm, Metric } from 'aws-cdk-lib/aws-cloudwatch';
+import { SnsAction } from 'aws-cdk-lib/aws-cloudwatch-actions';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Topic } from 'aws-cdk-lib/aws-sns';
+import { LambdaSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { join } from 'path';
 
 export class TsCwMetricsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const webHookLambda = new NodejsFunction(this, 'webHookLambda', {
+      runtime: Runtime.NODEJS_18_X,
+      handler: 'handler',
+      entry: (join(__dirname, '..', 'services', 'hook.ts'))
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TsCwMetricsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const alarmTopic = new Topic(this, 'TSAlarmTopic', {
+      displayName: 'TsAlarmTopic',
+      topicName: 'TSAlarmTopic'
+    });
+
+    alarmTopic.addSubscription(new LambdaSubscription(webHookLambda));
+
+    const sampleAlarm = new Alarm(this, 'Ts-ApiAlarm', {
+      metric: new Metric({
+          metricName: 'custom-error',
+          namespace: 'Custom',
+          period: cdk.Duration.minutes(1),
+          statistic: 'Sum',
+      }),
+      evaluationPeriods: 1,
+      threshold: 100,
+  });
+
+  const topicAction = new SnsAction(alarmTopic);
+  sampleAlarm.addAlarmAction(topicAction);
+  sampleAlarm.addOkAction(topicAction);
+
   }
 }
