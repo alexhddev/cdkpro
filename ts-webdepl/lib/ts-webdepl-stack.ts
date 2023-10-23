bb63dc15382ce41cf65a03f489ae87db37539698
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-10-23 12:09:42 +0300

    [Ts] Bucket hosting and CloudFront

--- ts-webdepl/lib/ts-webdepl-stack.ts
+++ ts-webdepl/lib/ts-webdepl-stack.ts
@@ -1,16 +1,47 @@
 import * as cdk from 'aws-cdk-lib';
+import { Distribution, OriginAccessIdentity } from 'aws-cdk-lib/aws-cloudfront';
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { existsSync } from 'fs';
+import { join } from 'path';
 
 export class TsWebdeplStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const deploymentBucket = new Bucket(this, 'TsWebDeploymentBucket')
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TsWebdeplQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const uiDir = join(__dirname, '..','..', 'web', 'dist');
+    if (!existsSync(uiDir)) {
+      console.warn('Ui dir not found: ' + uiDir);
+      return;
+    }
+
+    const originIdentity = new OriginAccessIdentity(this, 'OriginAccessIdentity');
+    deploymentBucket.grantRead(originIdentity);
+
+    const distribution = new Distribution(this, 'WebDeploymentDistribution', {
+      defaultRootObject: 'index.html',
+      defaultBehavior: {
+        origin: new S3Origin(deploymentBucket, {
+          originAccessIdentity: originIdentity
+        }),
+      },      
+    });
+
+    new BucketDeployment(this, 'WebDeployment', {
+      destinationBucket: deploymentBucket,
+      sources: [Source.asset(uiDir)],
+      distribution: distribution
+    });
+
+    new cdk.CfnOutput(this, 'TsAppUrl', {
+      value: distribution.distributionDomainName
+    })
+
+
+
   }
 }
