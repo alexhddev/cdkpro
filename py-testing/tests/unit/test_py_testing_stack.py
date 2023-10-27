cde61d1e591457e2f26e7828a0d46b1221496d86
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-10-27 16:02:38 +0300

    [PY] test matchers

--- py-testing/tests/unit/test_py_testing_stack.py
+++ py-testing/tests/unit/test_py_testing_stack.py
@@ -1,6 +1,7 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 import pytest
+from aws_cdk.assertions import Match
 
 from py_testing.py_testing_stack import PySimpleStack
 
@@ -17,3 +18,33 @@ def test_lambda_props(simple_template):
         "Runtime": "python3.11"
     })
     simple_template.resource_count_is("AWS::Lambda::Function", 1)
+
+def test_lambda_runtime_with_matcher(simple_template): 
+    simple_template.has_resource_properties("AWS::Lambda::Function", {
+        "Runtime": Match.string_like_regexp("python")
+    })
+
+def test_lambda_bucket_with_matchers(simple_template):
+    simple_template.has_resource_properties(
+        "AWS::IAM::Policy",
+        Match.object_like(
+            {
+                "PolicyDocument": {
+                    "Statement": [
+                        {
+                            "Resource": [
+                                {
+                                    "Fn::GetAtt": [
+                                        Match.string_like_regexp("SimpleBucket"),
+                                        "Arn",
+                                    ]
+                                },
+                                Match.any_value()
+                            ]
+                        }
+                    ]
+                }
+            }
+        ),
+    )
+
