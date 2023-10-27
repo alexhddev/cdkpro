b2887070e61bc00be72618ee59e5403da7b5e4d4
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-10-27 15:20:44 +0300

    [PY] first test

--- py-testing/tests/unit/test_py_testing_stack.py
+++ py-testing/tests/unit/test_py_testing_stack.py
@@ -1,10 +1,19 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
+import pytest
 
 from py_testing.py_testing_stack import PySimpleStack
 
-
-def test_sqs_queue_created():
+@pytest.fixture(scope="session")
+def simple_template():
     app = core.App()
     stack = PySimpleStack(app, "py-testing")
     template = assertions.Template.from_stack(stack)
+    return template
+
+
+def test_lambda_props(simple_template): 
+    simple_template.has_resource_properties("AWS::Lambda::Function", {
+        "Runtime": "python3.11"
+    })
+    simple_template.resource_count_is("AWS::Lambda::Function", 1)
