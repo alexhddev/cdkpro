31dc91dc9e61bbae8c4a71f25202b55a95101600
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-10-27 16:28:57 +0300

    [PY] test captors

--- py-testing/tests/unit/test_py_testing_stack.py
+++ py-testing/tests/unit/test_py_testing_stack.py
@@ -1,9 +1,10 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 import pytest
-from aws_cdk.assertions import Match
+from aws_cdk.assertions import Match, Capture
 
 from py_testing.py_testing_stack import PySimpleStack
+
 
 @pytest.fixture(scope="session")
 def simple_template():
@@ -13,16 +14,18 @@ def simple_template():
     return template
 
 
-def test_lambda_props(simple_template): 
-    simple_template.has_resource_properties("AWS::Lambda::Function", {
-        "Runtime": "python3.11"
-    })
+def test_lambda_props(simple_template):
+    simple_template.has_resource_properties(
+        "AWS::Lambda::Function", {"Runtime": "python3.11"}
+    )
     simple_template.resource_count_is("AWS::Lambda::Function", 1)
 
-def test_lambda_runtime_with_matcher(simple_template): 
-    simple_template.has_resource_properties("AWS::Lambda::Function", {
-        "Runtime": Match.string_like_regexp("python")
-    })
+
+def test_lambda_runtime_with_matcher(simple_template):
+    simple_template.has_resource_properties(
+        "AWS::Lambda::Function", {"Runtime": Match.string_like_regexp("python")}
+    )
+
 
 def test_lambda_bucket_with_matchers(simple_template):
     simple_template.has_resource_properties(
@@ -39,7 +42,7 @@ def test_lambda_bucket_with_matchers(simple_template):
                                         "Arn",
                                     ]
                                 },
-                                Match.any_value()
+                                Match.any_value(),
                             ]
                         }
                     ]
@@ -48,3 +51,18 @@ def test_lambda_bucket_with_matchers(simple_template):
         ),
     )
 
+
+def test_lambda_actions_with_captors(simple_template):
+    lambda_actions_captor = Capture()
+    simple_template.has_resource_properties(
+        "AWS::IAM::Policy",
+        {"PolicyDocument": {"Statement": [{"Action": lambda_actions_captor}]}},
+    )
+
+    expected_actions = [
+        "s3:GetBucket*",
+        "s3:GetObject*",
+        "s3:List*"
+       ]
+    
+    assert sorted(lambda_actions_captor.as_array()) == sorted(expected_actions)
