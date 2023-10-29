ee0fcc987eb2ab48c1987e2cae1d94b69c1daf7b
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-10-29 19:44:45 +0200

    [Ts] CDK tags

--- ts-others/bin/ts-others.ts
+++ ts-others/bin/ts-others.ts
@@ -4,18 +4,12 @@ import * as cdk from 'aws-cdk-lib';
 import { TsOthersStack } from '../lib/ts-others-stack';
 
 const app = new cdk.App();
-new TsOthersStack(app, 'TsOthersStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const othersStack = new TsOthersStack(app, 'TsOthersStack');
+cdk.Tags.of(othersStack).add('stage', 'test');
+cdk.Tags.of(othersStack).add('storage', 'main',{
+  includeResourceTypes: ['AWS::S3::Bucket']
+});
+cdk.Tags.of(othersStack).add('storage', 'aux',{
+  includeResourceTypes: ['AWS::S3::Bucket'],
+  priority: 150
 });
